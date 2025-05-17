3577f1d8b8a2b1f4a955c786ac8d0f5e8806eb80
LordHalizar <crosbyalexander42@protonmail.com>
2025-05-17 13:50:53 -0600

    Applied color changing decal to projectile and changed C++ coding around

--- Source/FirstPerson415/FirstPerson415Projectile.cpp
+++ Source/FirstPerson415/FirstPerson415Projectile.cpp
@@ -38,6 +38,17 @@ AFirstPerson415Projectile::AFirstPerson415Projectile()
 	InitialLifeSpan = 3.0f;
 }
 
+void AFirstPerson415Projectile::BeginPlay()
+{
+	Super::BeginPlay();
+	randColor = FLinearColor(UKismetMathLibrary::RandomFloatInRange(0.f, 1.f), UKismetMathLibrary::RandomFloatInRange(0.f, 1.f), UKismetMathLibrary::RandomFloatInRange(0.f, 1.f), 1.f);
+
+	dmiMat = UMaterialInstanceDynamic::Create(projMat, this);
+	ballMesh->SetMaterial(0, dmiMat);
+
+	dmiMat->SetVectorParameterValue("ProjColor", randColor);
+}
+
 void AFirstPerson415Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	// Only add impulse and destroy projectile if we hit a physics
@@ -50,12 +61,7 @@ void AFirstPerson415Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* Othe
 
 	if (OtherActor != nullptr)
 	{
-		float ranNUmX = UKismetMathLibrary::RandomFloatInRange(0.f, 1.f);
-		float ranNUmY = UKismetMathLibrary::RandomFloatInRange(0.f, 1.f);
-		float ranNUmZ = UKismetMathLibrary::RandomFloatInRange(0.f, 1.f);
 		float frameNum = UKismetMathLibrary::RandomFloatInRange(0.f, 3.f);
-
-		FVector4 randColor = FVector4(ranNUmX, ranNUmY, ranNUmZ, 1.f);
 
 		auto Decal = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), baseMat, FVector(UKismetMathLibrary::RandomFloatInRange(20.f, 40.f)), Hit.Location, Hit.Normal.Rotation(), 0.f);
 		auto MatInstance = Decal->CreateDynamicMaterialInstance();
